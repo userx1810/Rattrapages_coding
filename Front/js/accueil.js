0d22843dea6839443a36f439c10b03b72684bd2a
Stacy Ambois <stacy.ambois@edu.devinci.fr>
2024-06-30 22:44:35 +0200

    .

--- Front/js/accueil.js
+++ Front/js/accueil.js
@@ -8,98 +8,98 @@ const searchUrl = BASE_URL + "/search/movie?" + API_KEY;
 import Swiper from "/node_modules/swiper/swiper-bundle.min.js";
 
 const swiper = new Swiper(".swiper-container", {
-	slidesPerView: 1,
-	spaceBetween: 10,
-	loop: true,
-	pagination: {
-		el: ".swiper-pagination",
-		clickable: true,
-	},
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
 const genres = {
-	genres: [
-		{
-			id: 28,
-			name: "Action",
-		},
-		{
-			id: 12,
-			name: "Adventure",
-		},
-		{
-			id: 16,
-			name: "Animation",
-		},
-		{
-			id: 35,
-			name: "Comedy",
-		},
-		{
-			id: 80,
-			name: "Crime",
-		},
-		{
-			id: 99,
-			name: "Documentary",
-		},
-		{
-			id: 18,
-			name: "Drama",
-		},
-		{
-			id: 10751,
-			name: "Family",
-		},
-		{
-			id: 14,
-			name: "Fantasy",
-		},
-		{
-			id: 36,
-			name: "History",
-		},
-		{
-			id: 27,
-			name: "Horror",
-		},
-		{
-			id: 10402,
-			name: "Music",
-		},
-		{
-			id: 9648,
-			name: "Mystery",
-		},
-		{
-			id: 10749,
-			name: "Romance",
-		},
-		{
-			id: 878,
-			name: "Science Fiction",
-		},
-		{
-			id: 10770,
-			name: "TV Movie",
-		},
-		{
-			id: 53,
-			name: "Thriller",
-		},
-		{
-			id: 10752,
-			name: "War",
-		},
-		{
-			id: 37,
-			name: "Western",
-		},
-	],
+  genres: [
+    {
+      id: 28,
+      name: "Action",
+    },
+    {
+      id: 12,
+      name: "Adventure",
+    },
+    {
+      id: 16,
+      name: "Animation",
+    },
+    {
+      id: 35,
+      name: "Comedy",
+    },
+    {
+      id: 80,
+      name: "Crime",
+    },
+    {
+      id: 99,
+      name: "Documentary",
+    },
+    {
+      id: 18,
+      name: "Drama",
+    },
+    {
+      id: 10751,
+      name: "Family",
+    },
+    {
+      id: 14,
+      name: "Fantasy",
+    },
+    {
+      id: 36,
+      name: "History",
+    },
+    {
+      id: 27,
+      name: "Horror",
+    },
+    {
+      id: 10402,
+      name: "Music",
+    },
+    {
+      id: 9648,
+      name: "Mystery",
+    },
+    {
+      id: 10749,
+      name: "Romance",
+    },
+    {
+      id: 878,
+      name: "Science Fiction",
+    },
+    {
+      id: 10770,
+      name: "TV Movie",
+    },
+    {
+      id: 53,
+      name: "Thriller",
+    },
+    {
+      id: 10752,
+      name: "War",
+    },
+    {
+      id: 37,
+      name: "Western",
+    },
+  ],
 };
 const filmscontainer = document.getElementById("filmscontainer");
 const form = document.getElementById("form");
@@ -107,52 +107,52 @@ const search = document.getElementById("search");
 const actionbutton = document.getElementById("actionbutton");
 const adventurebutton = document.getElementById("adventurebutton");
 actionbutton.addEventListener("click", () => {
-	getFilmsOfGenre(28);
+  getFilmsOfGenre(28);
 });
 adventurebutton.addEventListener("click", () => {
-	getFilmsOfGenre(12);
+  getFilmsOfGenre(12);
 });
 
 function getFilm(url) {
-	fetch(url)
-		.then((res) => res.json())
-		.then((data) => {
-			displayFilm(data.results);
-			console.log(data.results);
-		})
-		.catch((error) => {
-			console.error("Error fetching films:", error);
-		});
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      displayFilm(data.results);
+      console.log(data.results);
+    })
+    .catch((error) => {
+      console.error("Error fetching films:", error);
+    });
 }
 
 function getFilmsOfGenre(genreId) {
-	const genreUrl = `${BASE_URL}discover/movie?api_key=${API_KEY}&with_genres=${genreId}`;
-	getFilm(genreUrl);
+  const genreUrl = `${BASE_URL}discover/movie?api_key=${API_KEY}&with_genres=${genreId}`;
+  getFilm(genreUrl);
 }
 
 function getFilmSwiper(url) {
-	fetch(url)
-		.then((res) => res.json())
-		.then((data) => {
-			displaySwiper(data.results);
-			console.log(data.results);
-		})
-		.catch((error) => {
-			console.error("Error fetching films:", error);
-		});
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      displaySwiper(data.results);
+      console.log(data.results);
+    })
+    .catch((error) => {
+      console.error("Error fetching films:", error);
+    });
 }
 
 function displaySwiper(films) {
-	const swiper = document.querySelector(".swiper-wrapper");
-	swiper.innerHTML = ""; // Clear previous content
+  const swiper = document.querySelector(".swiper-wrapper");
+  swiper.innerHTML = ""; // Clear previous content
 
-	films.forEach((film) => {
-		const { title, poster_path, overview, vote_average } = film;
+  films.forEach((film) => {
+    const { title, poster_path, overview, vote_average } = film;
 
-		const swiperSlide = document.createElement("div");
-		swiperSlide.classList.add("swiper-slide");
+    const swiperSlide = document.createElement("div");
+    swiperSlide.classList.add("swiper-slide");
 
-		swiperSlide.innerHTML = `
+    swiperSlide.innerHTML = `
       <img src="${IMG_URL}${poster_path}" alt="${title}" />
       <div class="swiper-slide-info">
         <h3>${title}</h3>
@@ -160,21 +160,21 @@ function displaySwiper(films) {
       </div>
     `;
 
-		swiper.appendChild(swiperSlide);
-	});
+    swiper.appendChild(swiperSlide);
+  });
 }
 
 function displayFilm(films) {
-	const filmscontainer = document.querySelector("#filmscontainer");
-	filmscontainer.innerHTML = ""; // Clear previous content
+  const filmscontainer = document.querySelector("#filmscontainer");
+  filmscontainer.innerHTML = ""; // Clear previous content
 
-	films.forEach((film) => {
-		const { title, poster_path, overview, vote_average } = film;
+  films.forEach((film) => {
+    const { title, poster_path, overview, vote_average } = film;
 
-		const filmCard = document.createElement("div");
-		filmCard.classList.add("film");
+    const filmCard = document.createElement("div");
+    filmCard.classList.add("film");
 
-		filmCard.innerHTML = `
+    filmCard.innerHTML = `
       <img src="${IMG_URL}${poster_path}" alt="${title}" />
 
       <div class="film-info">
@@ -188,8 +188,8 @@ function displayFilm(films) {
       </div>
     `;
 
-		filmscontainer.appendChild(filmCard);
-	});
+    filmscontainer.appendChild(filmCard);
+  });
 }
 
 getFilm(API_URL);
