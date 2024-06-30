21047e279c3163dfcbd567c7fe231ceb06003d46
Stacy Ambois <stacy.ambois@edu.devinci.fr>
2024-06-30 19:24:33 +0200

    swiper retiré

--- Front/js/accueil.js
+++ Front/js/accueil.js
@@ -5,18 +5,7 @@ const API_URL = `${BASE_URL}movie/popular?api_key=${API_KEY}`;
 const TOP_RATED = `${BASE_URL}movie/top_rated?api_key=${API_KEY}`;
 const searchUrl = BASE_URL + "/search/movie?" + API_KEY;
 
-import Swiper from "/swiper.js";
-
 // import styles bundle
-const swiper = new Swiper(".swiper-container", {
-	slidesPerView: 1,
-	spaceBetween: 10,
-	loop: true,
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-});
 
 const genres = {
 	genres: [
