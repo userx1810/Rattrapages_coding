894d71f3e75231f81a7426da674a6262475e05e6
Stacy Ambois <stacy.ambois@edu.devinci.fr>
2024-06-30 17:10:54 +0200

    swiper

--- Front/js/accueil.js
+++ Front/js/accueil.js
@@ -6,112 +6,112 @@ const API_URL = `${BASE_URL}movie/popular?api_key=${API_KEY}`;
 const searchUrl = BASE_URL + "/search/movie?" + API_KEY;
 
 const genres = {
-  genres: [
-    {
-      id: 28,
-      name: "Action",
-    },
-    {
-      id: 12,
-      name: "Adventure",
-    },
-    {
-      id: 16,
-      name: "Animation",
-    },
-    {
-      id: 35,
-      name: "Comedy",
-    },
-    {
-      id: 80,
-      name: "Crime",
-    },
-    {
-      id: 99,
-      name: "Documentary",
-    },
-    {
-      id: 18,
-      name: "Drama",
-    },
-    {
-      id: 10751,
-      name: "Family",
-    },
-    {
-      id: 14,
-      name: "Fantasy",
-    },
-    {
-      id: 36,
-      name: "History",
-    },
-    {
-      id: 27,
-      name: "Horror",
-    },
-    {
-      id: 10402,
-      name: "Music",
-    },
-    {
-      id: 9648,
-      name: "Mystery",
-    },
-    {
-      id: 10749,
-      name: "Romance",
-    },
-    {
-      id: 878,
-      name: "Science Fiction",
-    },
-    {
-      id: 10770,
-      name: "TV Movie",
-    },
-    {
-      id: 53,
-      name: "Thriller",
-    },
-    {
-      id: 10752,
-      name: "War",
-    },
-    {
-      id: 37,
-      name: "Western",
-    },
-  ],
+	genres: [
+		{
+			id: 28,
+			name: "Action",
+		},
+		{
+			id: 12,
+			name: "Adventure",
+		},
+		{
+			id: 16,
+			name: "Animation",
+		},
+		{
+			id: 35,
+			name: "Comedy",
+		},
+		{
+			id: 80,
+			name: "Crime",
+		},
+		{
+			id: 99,
+			name: "Documentary",
+		},
+		{
+			id: 18,
+			name: "Drama",
+		},
+		{
+			id: 10751,
+			name: "Family",
+		},
+		{
+			id: 14,
+			name: "Fantasy",
+		},
+		{
+			id: 36,
+			name: "History",
+		},
+		{
+			id: 27,
+			name: "Horror",
+		},
+		{
+			id: 10402,
+			name: "Music",
+		},
+		{
+			id: 9648,
+			name: "Mystery",
+		},
+		{
+			id: 10749,
+			name: "Romance",
+		},
+		{
+			id: 878,
+			name: "Science Fiction",
+		},
+		{
+			id: 10770,
+			name: "TV Movie",
+		},
+		{
+			id: 53,
+			name: "Thriller",
+		},
+		{
+			id: 10752,
+			name: "War",
+		},
+		{
+			id: 37,
+			name: "Western",
+		},
+	],
 };
-const main = document.getElementById("main");
+const filmscontainer = document.getElementById("filmscontainer");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
 function getFilm(url) {
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      displayFilm(data.results);
-      console.log(data.results);
-    })
-    .catch((error) => {
-      console.error("Error fetching films:", error);
-    });
+	fetch(url)
+		.then((res) => res.json())
+		.then((data) => {
+			displayFilm(data.results);
+			console.log(data.results);
+		})
+		.catch((error) => {
+			console.error("Error fetching films:", error);
+		});
 }
 
 function displayFilm(films) {
-  const main = document.querySelector("main");
-  main.innerHTML = ""; // Clear previous content
+	const filmscontainer = document.querySelector("#filmscontainer");
+	filmscontainer.innerHTML = ""; // Clear previous content
 
-  films.forEach((film) => {
-    const { title, poster_path, overview, vote_average } = film;
+	films.forEach((film) => {
+		const { title, poster_path, overview, vote_average } = film;
 
-    const filmCard = document.createElement("div");
-    filmCard.classList.add("film");
+		const filmCard = document.createElement("div");
+		filmCard.classList.add("film");
 
-    filmCard.innerHTML = `
+		filmCard.innerHTML = `
       <img src="${IMG_URL}${poster_path}" alt="${title}" />
 
       <div class="film-info">
@@ -125,19 +125,19 @@ function displayFilm(films) {
       </div>
     `;
 
-    main.appendChild(filmCard);
-  });
+		filmscontainer.appendChild(filmCard);
+	});
 }
 
 getFilm(API_URL);
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const searchTerm = search.value;
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const searchTerm = search.value;
 
-  if (searchTerm) {
-    getFilm(searchUrl + `&query` + searchTerm);
-  } else {
-    getFilm(API_URL);
-  }
+	if (searchTerm) {
+		getFilm(searchUrl + `&query` + searchTerm);
+	} else {
+		getFilm(API_URL);
+	}
 });
